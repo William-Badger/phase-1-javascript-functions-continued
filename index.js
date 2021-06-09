71427513096e34194564f51b64fdfd344bc9fadf
William Badger <wkbadger@gmail.com>
2021-06-09 11:40:38 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork('work from home');
+
+function wrapAdjective(b = '*') {
+    function result(a = 'special') {
+        return `You are ${b}${a}${b}!`;
+    }
+    return result;
+}
+
+wrapAdjective()('a hard worker')
+
+wrapAdjective('||')('a dedicated programmer');
